6406b72ec1efcb81ce5b28dddf36bd989a73b036
1552937 <51300628+1552937@users.noreply.github.com>
2021-04-27 10:10:52 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,13 +7,14 @@ from Equation import Equation, EquationError
 if __name__ == "__main__":
     equation = input("\nPlease type in a balanced equation.\nExample of format: CaCl+NaCO3+H2O-->NaCl+CaCO3\n").replace(" ", "").replace("=", "-->")
     # Everything after entering an equation goes in the try block.
-    #try:
-    my_equation = Equation(equation)
-    print("Reactants: "+str(my_equation.reactants))
-    print("Products: " + str(my_equation.products))
-    reactants_masses = {}
-    for r in my_equation.reactants:
-        reactants_masses[str(r)] = float(input("How much, in grams, is the input mass of " + str(r) + "?" + "\n"))
-    my_equation.solve(reactants_masses)
-    #except EquationError:
-     #   print("Error: Invalid equation syntax.")
+    try:
+        my_equation = Equation(equation)
+        print("Reactants: "+str(my_equation.reactants))
+        print("Products: " + str(my_equation.products))
+        reactants_masses = {}
+        for r in my_equation.reactants:
+            reactants_masses[str(r)] = float(input("How much, in grams, is the input mass of " + str(r) + "?" + "\n"))
+        my_equation.solve(reactants_masses)
+    except EquationError:
+        print("Error: Invalid equation syntax.")
+        print("I mean its still an error but its prolly just cuz we screwed up programing it")
